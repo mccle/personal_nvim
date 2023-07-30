773f578277457f2ea3d7951dd26b2b6eff62ece3
Mason Cleveland <mccleve@umich.edu>
2023-07-29 23:13:22 -0400

    prepare for nvchad

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 repo_root=$(pwd)
 install_prefix=${repo_root}/usr
 
-cd neovim
+if [ ! -d ${install_prefix} ]; then
+    mkdir ${install_prefix}
+fi
+
 if [ -d ${install_prefix}/bin ]; then
 	echo "nvim is already compiled"
 else
     echo "nvim is not compiled yet, running make now"
+    cd neovim
     make CMAKE_INSTALL_PREFIX=${install_prefix}
     make install
 fi
 
-cd ${cwd}
+cd ${repo_root}
+
